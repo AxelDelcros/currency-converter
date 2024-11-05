ac518dc1ed9a778c4461caca5e21fa3cb9985ec8
Axel Delcros <axel.delcros@gmail.com>
2024-11-05 14:22:12 +0100

    Feat: Add rate input + update + conversion

---
+++ src/utils/format.ts
@@ -0,0 +1,21 @@
+export const getCleanValue = (rawValue: string) => {
+  // Replace comma by a dot + remove all characters that are not numbers or dots
+  const cleanedValue = rawValue.replace(",", ".").replace(/[^0-9.]/g, "")
+
+  // Remove all but the first dot
+  const splitValue = cleanedValue.split(".")
+  if (splitValue.length < 2) {
+    return cleanedValue
+  }
+  const newValue =
+    splitValue.slice(0, 2).join(".") + splitValue.slice(2).join("")
+
+  return newValue
+}
+
+export const getValueConversion = (value: string, currentRate: number) => {
+  const cleanValue = parseFloat(getCleanValue(value)) || 0
+  const newValue = currentRate * cleanValue
+  const newValuePrecision = newValue.toFixed(2)
+  return newValuePrecision
+}
--- todo.md
+++ todo.md
@@ -3,11 +3,11 @@
 ## Features
 
 - [x] Set up project + basic layout
-- [ ] Add real rate + update
-- [ ] Create Input with real time conversion
+- [x] Add real rate + update
+- [x] Create Input with real time conversion
+- [ ] Create in / out currency switch
 - [ ] Create History with validated conversions (max 5 entries)
 - [ ] Save conversion in history with a button
-- [ ] Create in / out currency switch
 - [ ] Add custom colors + typography
 - [ ] Add fixed rate checkbox + input
 - [ ] Manage fixed rate if set (with the 2% diff)
